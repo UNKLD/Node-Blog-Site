a9c8d5f5b6460664a69ef51b7654581b1fbfb7df
UNKLD <leuldamtew@yahoo.com>
2022-03-07 22:39:34 -0800

    add delete post

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const { result } = require("lodash");
 var _ = require("lodash");
 const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 try {
   mongoose.connect("mongodb://localhost:27017/blogDB");
@@ -23,48 +31,61 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const app = express();
 
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.get("/", async (_req, res) => {
   const posts = await Blog.find();
   res.render("home", { startingContent: homePageContent, posts });
 });
 
-app.get("/about", async (_req, res) => {
+app.get("/about", (_req, res) => {
   res.render("about", { aboutContent });
 });
 
-app.get("/contact", async (_req, res) => {
+app.get("/contact", (_req, res) => {
   res.render("contact", { contactContent });
 });
 
-app.get("/compose", (_req, res) => {
-  res.render("compose");
-});
-
-app.post("/compose", (req, res) => {
-  const post = new Blog({
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  });
-  post.save();
-  res.redirect("/");
-});
-
-app.get("/posts/:postTitle", async (req, res) => {
-  const postTitle =_.capitalize(req.params.postTitle);
-  const post = await Blog.find({ title: postTitle });
+app.get("/posts/:postId", async (req, res) => {
+  const {postId} = req.params
+  const post = await Blog.find({ _id: postId });
   if (post.length > 0) {
     res.render("post", { post });
   } else {
     res.render("404");
   }
 });
+
+
+app.route("/compose")
+  .get((_req, res) => {
+    res.render("compose");
+
+  })
+  .post(async (req, res) => {
+    const post = new Blog({
+      title: req.body.postTitle,
+      content: req.body.postBody,
+    });
+    await post.save();
+    res.redirect("/");
+  });
+
+app.post("/post/delete", async (req, res) => {
+  const { postId } = req.body;
+  try {
+    const post = await Blog.deleteOne({ _id: postId });
+    if (post.deletedCount === 0) {
+      res.json({
+        message: "Post Not Found",
+        result: post,
+      });
+    }
+    res.redirect("/");
+  } catch (error) {
+    res.send(error);
+  }
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
